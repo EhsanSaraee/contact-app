95e1242e111b1904a30e014d416845f3478085c3
ehsan <ehsansaraee6570@yahoo.com>
2022-05-24 17:42:31 +0430

    routing app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { AddEditUser, Home, UserInfo } from 'pages';
 import { FC } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App: FC = () => {
    return (
       <>
-         <h1>Contact App</h1>
+         <ToastContainer />
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/add" element={<AddEditUser />} />
+            <Route path="/update/:id" element={<AddEditUser />} />
+            <Route path="/view/:id" element={<UserInfo />} />
+         </Routes>
       </>
    );
 };
---
+++ src/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as Home } from 'pages/Home/Home';
+export { default as UserInfo } from 'pages/UserInfo/UserInfo';
+export { default as AddEditUser } from 'pages/AddEditUser/AddEditUser';
